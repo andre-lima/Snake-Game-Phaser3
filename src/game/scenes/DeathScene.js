88f4c56db4320654da725100eb884333d4e9b07a
Andre Dantas <andre.dantas@globaleagle.com>
2017-08-24 12:57:24 -0400

    Adding numbers and letters to buttons

--- src/game/scenes/DeathScene.js
+++ src/game/scenes/DeathScene.js
@@ -4,8 +4,10 @@ import config from '../utils/config';
 export default class extends Phaser.Scene {
 
   preload() {
+    const deathText = `ur ded !!1! lol`
+
     manager.restartPoints();
-    const text = this.add.text(config.width * 0.5, config.height * 0.5, 'You\'re ded!').setFont('48px silkscreen').setFill(config.textColor);
+    const text = this.add.text(config.width * 0.5, config.height * 0.5, deathText).setFont('48px silkscreen').setFill(config.textColor);
     text.setOrigin(0.5, 0.5);
 
     setTimeout(() => this.scene.start('Game'), 1000);
