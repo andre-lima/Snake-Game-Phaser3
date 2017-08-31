604e2c58a8682fec7fd1d5a343f013f996351039
Andre Dantas <andre.dantas.lima@gmail.com>
2017-08-30 22:48:29 -0400

    Adding images and preparing apk release

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,4 +1,5 @@
 import Phaser from '../../vendor/phaser';
+import config from '../utils/config';
 import food from '../components/Food/assets/food.gif';
 import head from '../components/Snake/assets/head.gif';
 import body from '../components/Snake/assets/body.gif';
@@ -9,10 +10,19 @@ export default class extends Phaser.Scene {
     this.load.image('food', food);
     this.load.image('head', head);
     this.load.image('body', body);
-  }
 
-  update() { 
-     this.scene.start('Game');
+    const txt1 = 'Hungry';
+    const txt2 = 'Retro';
+    const txt3 = 'Pixel';
+    const txt4 = 'Snake';
+
+    this.add.text(config.width * 0.5, config.height * 0.30, txt1).setFont('64px silkscreen').setFill(config.pixelColor).setOrigin(0.5, 0.5);
+    this.add.text(config.width * 0.5, config.height * 0.40, txt2).setFont('64px silkscreen').setFill(config.pixelColor).setOrigin(0.5, 0.5);
+    this.add.text(config.width * 0.5, config.height * 0.50, txt3).setFont('64px silkscreen').setFill(config.pixelColor).setOrigin(0.5, 0.5);
+    this.add.text(config.width * 0.5, config.height * 0.60, txt4).setFont('64px silkscreen').setFill(config.pixelColor).setOrigin(0.5, 0.5);
+    this.add.text(config.width * 0.5, config.height * 0.80, 'loading...').setFont('30px silkscreen').setFill(config.pixelColor).setOrigin(0.5, 0.5);
+    
+    setTimeout(() => this.scene.start('Game'), 3000);
   }
 
 }
